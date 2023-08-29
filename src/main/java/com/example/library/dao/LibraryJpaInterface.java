acba88f466d6a0e10bfa4e9586988796d7a249a3
kartik ivaturi <kartikivaturi@kartiks-Mac-mini.local>
2023-08-29 11:16:54 +0530

    updated properties

--- src/main/java/com/example/library/dao/LibraryJpaInterface.java
+++ src/main/java/com/example/library/dao/LibraryJpaInterface.java
@@ -1,6 +1,7 @@
 package com.example.library.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.library.entity.LibraryEntity;
 
